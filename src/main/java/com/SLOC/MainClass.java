7cf8c3b8b60683fdb2174dcb0f901e00e485e4e7
khyati389 <khyatikhyati389@gmail.com>
2020-02-15 21:24:51 -0500

    update executable JAR and add readme.txt

--- src/main/java/com/SLOC/MainClass.java
+++ src/main/java/com/SLOC/MainClass.java
@@ -1,20 +1,15 @@
 package com.SLOC;
 
-import java.util.Arrays;
-
 /**
  * @author Khyatibahen Chaudhary
  * *** Source Lines Of Codes SLOC Program ****
  * ********** Requirements ***********
- * 1. The total number of Java files. >done
- * 2. The total number of unique Java files. >done
- * 3. The total number of blank lines. (in all unique java files) >done
- * 4. The total number of comment lines. (in all unique java files) >done
- * 5. The total number of code lines. (only code line excluding comments and blanks) >done
- * 6. Add user enabled input
- * 7. Refactor code & remove unused variables
- * 8. Create a jar
- * 9. verify
+ * 1. The total number of Java files.
+ * 2. The total number of unique Java files.
+ * 3. The total number of blank lines. (in all unique java files)
+ * 4. The total number of comment lines. (in all unique java files)
+ * 5. The total number of code lines. (only code line excluding comments and blanks)
+ * 6. Create a jar
  */
 
 public class MainClass {
